8efa7d8971739dedaf11e27f48aefd297079a933
aguemes <alexgj93@gmail.com>
2018-08-14 16:30:00 +0200

    New features added to maclea

--- maclea.py
+++ maclea.py
@@ -9,3 +9,14 @@ def plot_decision_regions(x, y, classifier, test_idx=None, resolution=0.02)
     markers = ('s', 'x', 'o', '^', 'v')
     colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
     cmap = ListedColormap(colors[:len(np.unique(y))])
+    
+    # plot the decision surface
+    x1_min, x1_max = x[:, 0].min() - 1, x[:, 1].max() + 1
+    x2_min, x2_max = x[:, 0].min() - 1, x[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    z = z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+    
